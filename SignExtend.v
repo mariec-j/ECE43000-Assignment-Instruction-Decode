6d880c2a58d0bb085992b82175addf78f02d45e9
Naomi-ANN <nevellalex@gmail.com>
2026-03-22 17:04:49 -0700

    Update SignExtend.v

--- SignExtend.v
+++ SignExtend.v
@@ -1,6 +1,15 @@
+/*
+Input:
++signExtIn[15:0]
+
+Output:
+-signExtOut[31:0]
+*/
 `timescale 1ns / 1ps
 
 module SignExtend(
-
+input signExtIn[15:0]
+output signExtOut[31:0]
     );
+    
 endmodule
