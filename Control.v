040ce5cc776925a14c98af751489897af0a8e94d
mariec-j <mcjardiniano@cpp.edu>
2026-03-22 18:56:23 -0700

    Update Control.v and Register.v
    - added inputs and outputs for Control.v and RegisterFile.v module files

--- Control.v
+++ Control.v
@@ -1,6 +1,12 @@
 `timescale 1ns / 1ps
 
 module Control(
+    input [5:0] controlInput,
+    output reg [1:0] countrol_WB, control_ALUOp,
+    output reg [2:0] control_M,
+    output reg ALUSrc, RegDst
+);
 
-    );
+
+
 endmodule
--- RegisterFile.v
+++ RegisterFile.v
@@ -1,6 +1,12 @@
 `timescale 1ns / 1ps
 
 module RegisterFile(
+    input [5:0] readReg1, readReg2, writeReg,
+    input [31:0] writeData,
+    input RegWrite, clk, rst
+    
+    output reg [31:0] RegFile_readData1, RegFile_readData2
+);
 
-    );
+
 endmodule
