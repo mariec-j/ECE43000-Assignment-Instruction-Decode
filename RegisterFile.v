09dfa20f58f13e0aaa65798d0f1fb2ef57b835ca
mariec-j <mcjardiniano@cpp.edu>
2026-03-23 15:27:41 -0700

    Update RegisterFile.v
    - Added initial values for the general-purpose register for testing purposes

--- RegisterFile.v
+++ RegisterFile.v
@@ -12,7 +12,20 @@ module RegisterFile(
     
     output reg [31:0] RegFile_readData1, RegFile_readData2
 );
-reg [31:0] gen_purpose_reg [4:0]; // the general purpose registers of width 32 bits depth 32
+reg [31:0] gen_purpose_reg [31:0]; // the general purpose registers of width 32 bits depth 32
+initial begin
+gen_purpose_reg[0] = 32'h00000000;
+gen_purpose_reg[1] = 32'h00000001;
+gen_purpose_reg[2] = 32'h00000002;
+gen_purpose_reg[3] = 32'h00000003;
+gen_purpose_reg[4] = 32'h00000004;
+gen_purpose_reg[5] = 32'h00000005;
+gen_purpose_reg[6] = 32'h00000006;
+gen_purpose_reg[7] = 32'h00000007;
+gen_purpose_reg[8] = 32'h00000008;
+gen_purpose_reg[9] = 32'h00000009;
+gen_purpose_reg[10] = 32'h0000000a;
+end
 
 always @(posedge clk or negedge rst) begin
     if(!rst) begin 
